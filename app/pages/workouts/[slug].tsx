e3d5a0a0d25791ec0828f8fc9e537d28c199ab73
Jake Bayliss <bayliss.jw@gmail.com>
2023-08-05 20:51:21 +1000

    Rename workout content folder to 'content' because it was confusing me -.-

--- app/pages/workouts/[slug].tsx
+++ app/pages/workouts/[slug].tsx
@@ -10,9 +10,9 @@ const Index = (props: any) => {
   return (
     <main>
       <div className='p-5'>
-        <h1 className='my-3 font-bold text-2xl'>Week {props.data.attributes.week} - {props.data.attributes.title}</h1>
+        <h1 className='my-3 font-bold text-2xl'>Week {props.data?.attributes.week} - {props.data?.attributes.title}</h1>
         <div>
-          <Markdown>{props.data.body}</Markdown>
+          <Markdown>{props.data?.body}</Markdown>
         </div>
       </div>
       <Footer currentPageNumber={1} nextPage='week1-arms' previousPage='week1-welcome' />
@@ -22,7 +22,7 @@ const Index = (props: any) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   if(context.params) {
-    var content = (await fs.readFile(`./workouts/${context.params.slug}`)).toString();
+    var content = (await fs.readFile(`./content/${context.params.slug}.md`)).toString();
     var data = matter(content) as FrontMatterResult<IWorkout>;
 
     return {
@@ -38,11 +38,16 @@ export const getStaticProps: GetStaticProps = async (context) => {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const folder = path.join(process.cwd(), './workouts')
+  const folder = path.join(process.cwd(), './content')
   const filenames = await fs.readdir(folder);
+  console.log(filenames);
+  const slugs = filenames.map(s => s.replace('.md', ''));
 
   return {
-    paths: filenames.map((s) => ({ params: { slug: s }})),
+    paths: slugs.map(s => ({
+      params: {
+        slug: s
+      }})),
     fallback: false
   }
 }
