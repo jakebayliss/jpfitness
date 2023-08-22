455943580d5f611ff121a6266a49aebf32f79d3f
Jake Bayliss <bayliss.jw@gmail.com>
2023-08-22 16:30:24 +1000

    Remove bicep files

--- .github/clean.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item .\azuredeploy.parameters.json -Force  -ErrorAction Ignore
-Remove-Item .\bicep\*.json -Force  -ErrorAction Ignore
-Remove-Item .\arm-Ttk -Force -Recurse  -ErrorAction Ignore
-Remove-Item .\arm-Ttk.zip -Force  -ErrorAction Ignore
